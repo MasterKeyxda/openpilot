d6ddd8722ce5a7a38194971d0caf8a3288e07b33
keyur <keyur.tarunbhai@gmail.com>
2019-10-23 15:46:41 -0700

    Port in use USB

--- panda/python/__init__.py
+++ panda/python/__init__.py
@@ -183,6 +183,7 @@ class Panda(object):
                 self.bootstub = device.getProductID() == 0xddee
                 self.legacy = (device.getbcdDevice() != 0x2300)
                 self._handle = device.open()
+                self._handle.setAutoDetachKernelDriver(True) # Keyur edit; stupid usb driver not available
                 if claim:
                   self._handle.claimInterface(0)
                   #self._handle.setInterfaceAltSetting(0, 0) #Issue in USB stack
