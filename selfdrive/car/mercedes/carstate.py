fef072749f861d3109e3b76f6447761bbab51cdc
Master_Key <keyur.tarunbhai@gmail.com>
2019-10-31 15:40:19 -0700

    Change driver belt to value

--- selfdrive/car/mercedes/carstate.py
+++ selfdrive/car/mercedes/carstate.py
@@ -147,7 +147,7 @@ class CarState():
     self.prev_right_blinker_on = self.right_blinker_on
     self.left_blinker_on = cp.vl["DRIVER_CONTROL"]['BLINKER_LEFT'] == 1
     self.right_blinker_on = cp.vl["DRIVER_CONTROL"]['BLINKER_RIGHT'] == 1
-    self.seatbelt_unlatched = cp.vl["SEATBELT"]['DRIVER_BELTs'] == 1 #true if the bit value is 1
+    self.seatbelt_unlatched = cp.vl["SEATBELT"]['DRIVER_BELT'] #true if the bit value is 1
     self.steer_torque_driver = cp.vl["STEER_TORQUE"]['STEER_TORQUE']
     self.acc_active = cp.vl["CRUISE_CONTROL"]['CRUISE_ON'] # Check with the bit value, true if 1
     self.main_on = cp.vl["CRUISE_CONTROL"]['CRUISE_ON'] # Check with the bit value, true if 1
