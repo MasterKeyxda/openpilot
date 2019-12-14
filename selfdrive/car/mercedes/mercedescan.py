79c22ab52e9611a0ce5a6812d8644dafbaa4ff35
MasterKeyxda <keyur.tarunbhai@gmail.com>
2019-12-13 21:56:36 -0800

    Added mercedes checksum function

--- selfdrive/car/mercedes/mercedescan.py
+++ selfdrive/car/mercedes/mercedescan.py
@@ -5,11 +5,6 @@ from selfdrive.car.mercedes.values import CAR
 VisualAlert = car.CarControl.HUDControl.VisualAlert
 
 """
-def subaru_checksum(packer, values, addr):
-  dat = packer.make_can_msg(addr, 0, values)[2]
-  return (sum(dat[1:]) + (addr >> 8) + addr) & 0xff
-
-
 def subaru_checksum(data):
   print(data)
   checksum = 0xFF
@@ -36,14 +31,42 @@ def subaru_checksum(data):
       checksum = bit_sum
       shift = shift >> 1
   return ~checksum & 0xFF
-"""
+
 def subaru_checksum(packer, values, addr):
   dat = packer.make_can_msg(addr, 0, values)[2]
   print(dat)
   return (sum(dat[1:]) + (addr >> 8) + addr) & 0xff
+"""
 
-#def subaru_checksum(packer, values, addr):
+def subaru_checksum(packer, values, addr):
+  #KEYUR EDITED FUNCTION
+  data = packer.make_can_msg(addr, 0, values)[2]
+  checksum = 0xFF
+  temp_chk = 0
+  bit_sum = 0
 
+  for byte in data:
+    shift = 0x80
+    for i in range(8, 0, -1):
+      bit_sum = byte & shift
+      temp_chk = checksum & 0x80
+      if bit_sum != 0:
+        bit_sum = 0x1C
+        if (temp_chk != 0):
+          bit_sum = 1
+        checksum = checksum << 1
+        temp_chk = checksum | 1
+        bit_sum ^= temp_chk
+      else:
+        if temp_chk != 0:
+          bit_sum = 0x1D
+        checksum = checksum << 1
+        bit_sum ^= checksum
+      checksum = bit_sum
+      shift = shift >> 1
+  return ~checksum & 0xFF
+
+"""
 def create_steering_control(packer, car_fingerprint, apply_steer, frame, steer_step):
 
   if car_fingerprint == CAR.ECLASS:
@@ -63,6 +86,25 @@ def create_steering_control(packer, car_fingerprint, apply_steer, frame, steer_s
     #values["Checksum"] = subaru_checksum(values)
     values["Checksum"] = subaru_checksum(packer, values, 0x122)
   return packer.make_can_msg("ES_LKAS", 0, values)
+"""
+
+def create_steering_control(packer, car_fingerprint, apply_steer, frame, steer_step):
+  #KEYUR EDITED FUNCTION
+  if car_fingerprint == CAR.ECLASS:
+    #counts from 0 to 15 then back to 0 + 16 for enable bit
+    print('--------------------HELP--------------------')
+    idx = ((frame // steer_step) % 16)
+    print(apply_steer)
+    print(frame)
+    print(steer_step)
+    print(idx)
+    values = {
+      "Counter": idx,
+      "STEER_TORQUE": apply_steer,
+    }
+    #values["Checksum"] = subaru_checksum(values)
+    values["Checksum"] = subaru_checksum(packer, values, 14)
+  return packer.make_can_msg("STEERING_1", 0, values)
 """
 def create_steering_status(packer, car_fingerprint, apply_steer, frame, steer_step):
   
